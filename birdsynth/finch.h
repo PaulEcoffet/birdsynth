eb11111cb7e7a79670721d6b258babc35ab785ae
Paul Ecoffet <ecoffet.paul@gmail.com>
2017-03-15 16:11:05 +0100

    Cython implementation of birdsynth

---
+++ birdsynth/finch.h
@@ -0,0 +1,4 @@
+#ifndef FINCH_H
+#define FINCH_H
+void finch(double *alpha, double *beta, int size, double *out);
+#endif
---
+++ birdsynth/rk4.h
@@ -0,0 +1,5 @@
+#ifndef RK4_H
+#define RK4_H
+void rk4(void deri(int , double [], double [], double ),
+double h[], int n, double t, double dt);
+#endif
