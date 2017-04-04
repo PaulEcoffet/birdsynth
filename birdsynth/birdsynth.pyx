aa1addf60af6be68b7c9bf54442f80ef82b43de8
Paul Ecoffet <ecoffet.paul@gmail.com>
2017-04-04 16:51:51 +0200

    release gil

--- birdsynth/birdsynth.pyx
+++ birdsynth/birdsynth.pyx
@@ -1,7 +1,7 @@
 from cython cimport boundscheck, wraparound
 
 cdef extern from "finch.h":
-  void finch(double *alpha, double *beta, int size, double *out)
+  void finch(double *alpha, double *beta, int size, double *out) nogil
 
 import numpy as np
 cimport numpy as np
@@ -11,12 +11,16 @@ np.import_array()
 DTYPE = np.double
 ctypedef np.double_t DTYPE_t
 
+@boundscheck(False)
+@wraparound(False)
 def synth(np.ndarray[DTYPE_t, ndim=2, mode="c"] alphabeta not None):
+  """Synthesize a song with the alpha and beta parameters."""
   assert alphabeta.dtype == DTYPE, "Must be double"
-  cdef double[:] alpha = np.asarray(alphabeta[:, 0]).astype(DTYPE)
-  cdef double[:] beta = np.asarray(alphabeta[:, 1]).astype(DTYPE)
+  cdef double[:] alpha = alphabeta[:, 0].astype(DTYPE)
+  cdef double[:] beta = alphabeta[:, 1].astype(DTYPE)
   cdef int size = alphabeta.shape[0]
 
   cdef np.ndarray[DTYPE_t, ndim=1] out = np.zeros(size-2, dtype=DTYPE)
-  finch(&alpha[0], &beta[0], size, &out[0])
+  with nogil:
+    finch(&alpha[0], &beta[0], size, &out[0])
   return out
--- setup.py
+++ setup.py
@@ -4,15 +4,12 @@ from Cython.Build import cythonize
 
 
 ext_modules = [Extension('birdsynth', ['birdsynth/birdsynth.pyx',
-                                  'birdsynth/finch.c',
-                                  'birdsynth/rk4.c'])]
+                                       'birdsynth/finch.c',
+                                       'birdsynth/rk4.c'])]
 
 setup(
     name='birdsynth',
     version='1.1',
     include_dirs=[np.get_include()],  # Add Include path of numpy
-    ext_modules=cythonize(ext_modules)
+    ext_modules=cythonize(ext_modules),
 )
-
-
-sourcefiles = ['example.pyx', 'helper.c', 'another_helper.c']
